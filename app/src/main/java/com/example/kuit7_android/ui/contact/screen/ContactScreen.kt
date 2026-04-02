af02e8985ca4a5fec700d316a226e5109b753e5c
mwpark <hyminwoo0000@gmail.com>
2026-04-02 19:35:19 +0900

    week3 complete

--- app/src/main/java/com/example/kuit7_android/ui/contact/screen/ContactScreen.kt
+++ app/src/main/java/com/example/kuit7_android/ui/contact/screen/ContactScreen.kt
@@ -1,7 +1,5 @@
 package com.example.kuit7_android.ui.contact.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,15 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
