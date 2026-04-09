201aad05308b3fbf98ef19fa9ec52154aee5db30
acturus <kevin480703@gmail.com>
2026-04-09 21:42:02 +0900

    week4 mission

--- app/src/main/java/com/example/kuit7_android/navigation/BottomNavBar.kt
+++ app/src/main/java/com/example/kuit7_android/navigation/BottomNavBar.kt
@@ -1,28 +1,20 @@
 package com.example.kuit7_android.navigation
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.example.kuit7_android.ui.profile.component.NavItem
-import com.example.kuit7_android.ui.theme.KuitTheme
+import com.example.kuit7_android.ui.components.NavItem
 
 @Composable
 fun BottomNavBar(
